ab72de8f98fcbd2d31a96a3fad03a4c57fe1adda
ArslanArshad105 <60978049+ArslanArshad105@users.noreply.github.com>
2024-09-13 19:19:58 +0500

    Created Skills Section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import About from "@/components/about/about";
+import SkillsSection from "@/components/skillsSection/SkillsSection";
 
 export default function Home() {
   return (
-    // <div className={styles.page}>
-    <About />
-
-    // </div>
+    <>
+      <About />
+      <SkillsSection />
+    </>
   );
 }
--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,14 +1,14 @@
 import React from "react";
 import {
   Container,
-  Grid,
   Typography,
   Box,
   Button,
   Card,
   CardContent,
 } from "@mui/material";
-import "./about.css"; // Import the CSS file for the animation
+import Grid from "@mui/material/Grid2";
+import "./about.css";
 
 const data = [
   {
@@ -29,52 +29,63 @@ const About = () => {
   return (
     <Box
       sx={{
-        background:
-          "linear-gradient(90deg,#07233b 1.67%,#041d34 39.95%,#04192d 58.24%,#040e18 80.28%,#050c16 101.48%)",
-        padding: "70px 0",
+        padding: "100px 0",
         position: "relative",
       }}
     >
       <Container>
         <Grid container alignItems="center">
           {/* Image Section */}
-          <Grid item xs={12} md={6}>
-            <Box sx={{ position: "relative", textAlign: "center" }}>
-              <img
+          <Grid size={{ xs: 12, md: 7 }}>
+            <Box
+              sx={{
+                position: "relative",
+                textAlign: "center",
+              }}
+            >
+              <Box
                 style={{
                   position: "relative",
                   zIndex: "1",
-                  width: "100%",
-                  height: "auto",
-                  maxHeight: "500px",
-                  maxWidth: "400px",
                 }}
+                component="img"
                 alt="Profile Picture"
                 src="assets/media/images/about-banner.png"
+                sx={{
+                  width: { xs: "80%", md: "50%", lg: "60%", xl: "80%" },
+                  height: { xs: "100%", md: "50%", lg: "100%", xl: "100%" },
+                }}
               />
               {/* SVG Image */}
-              <img
+              <Box
+                component="img"
                 src="assets/media/images/effect-1.svg"
                 alt="SVG Effect"
-                className="rotating-svg"
+                className="rotating-btm"
+                sx={{
+                  left: { xs: "-60px", md: "0px", lg: "0px", xl: "-60px" },
+                  bottom: {
+                    xs: "-40px",
+                    md: "-40px",
+                    lg: "-40px",
+                    xl: "-40px",
+                  },
+                }}
               />
             </Box>
           </Grid>
           {/* Text Section */}
           <Grid
-            item
-            xs={12}
-            md={6}
+            size={{ xs: 12, md: 5 }}
             sx={{
-              marginTop: { xs: "45px", md: "0" }, // Add margin top on mobile
+              marginTop: { xs: "45px", md: "45px", lg: "0px", xl: "0px" },
+              marginLeft: { xs: "0px", md: "-40px", lg: "-70px", xl: "0px" },
             }}
           >
             <img
               src="assets/media/images/effect-1.svg"
               alt="SVG Effect"
               className="rotating-top"
-              // height={300}
-              // width={500}
             />
             <Typography
               variant="overline"
@@ -82,24 +93,29 @@ const About = () => {
                 color: "#fff",
                 letterSpacing: "1px",
                 backgroundColor: "#0788ff33",
-                padding: "5px 12px",
+                padding: "12px 16px",
                 borderRadius: "4px",
+                fontSize: "14px",
                 fontWeight: "bold",
               }}
             >
-              ABOUT US
+              ABOUT
             </Typography>
             <Typography
               variant="h4"
-              sx={{ color: "#ffffff", fontWeight: "bold", marginTop: "16px" }}
+              sx={{
+                color: "#ffffff",
+                fontWeight: "bold",
+                marginTop: "16px",
+                fontSize: "40px",
+              }}
             >
-              I AM AVAILABLE FOR <br />
-              <span style={{ color: "#0085ff" }}>UI</span>{" "}
-              <span style={{ color: "#0040ff" }}>UX DESIGN</span> PROJECTS
+              I AM AVAILABLE FOR
+              <span style={{ color: "#0085ff" }}> UI UX DESIGN</span> PROJECTS
             </Typography>
             <Typography
               variant="body1"
-              sx={{ color: "#8f99b3", marginTop: "16px" }}
+              sx={{ color: "#fff", marginTop: "30px" }}
             >
               The standard chunk of Lorem Ipsum used since the 1500s is
               reproduced below for those interested. Sections 1.10.32 and
@@ -108,18 +124,18 @@ const About = () => {
               versions from the 1914 translation by H. Rackham.
             </Typography>
 
-            <Grid container spacing={2} sx={{ marginTop: "24px" }}>
+            <Grid container spacing={2} sx={{ marginTop: "30px" }}>
               {data.map((val, index) => (
-                <Grid item xs={4} key={index}>
+                <Grid size={{ xs: 4 }} key={index}>
                   <Card
                     sx={{
                       backgroundColor: "#ffffff0d",
                       color: "#fff",
                       padding: "10px",
                       borderRadius: "8px",
-                      border: "1px solid #ffffff26", // Initial border
+                      border: "1px solid #ffffff26",
                       "&:hover": {
-                        borderColor: "#0085ff", // Change border color on hover
+                        borderColor: "#0085ff",
                       },
                     }}
                   >
@@ -157,13 +173,13 @@ const About = () => {
               sx={{
                 backgroundColor: "#0085ff",
                 border: "1px solid #0085ff",
-                marginTop: "24px",
+                marginTop: "50px",
                 borderRadius: "24px",
                 padding: "10px 24px",
                 textTransform: "none",
                 "&:hover": {
                   border: "1px solid #fff",
-                  backgroundColor: "#040c16", // Change border color on hover
+                  backgroundColor: "#040c16",
                 },
               }}
             >
